6c8ce85cd04af75b8828c304031c8d49cac5e0bd
Peter Polidoro <peterpolidoro@gmail.com>
2021-04-16 17:37:16 -0400

    Add subplot titles and sort by title

--- lickport_array/lickport_array.py
+++ lickport_array/lickport_array.py
@@ -25,20 +25,23 @@ class LickportArray():
     def __init__(self,*args,**kwargs):
         base_path = pathlib.Path(__file__).resolve().parent
         data_path = base_path.joinpath('data')
-        self.runs = []
+        self.runs = {}
         for data_file in data_path.iterdir():
+            data_file_stem = data_file.stem
             run = numpy.genfromtxt(data_file,delimiter=',')
             run = run[:,[3,4]]
-            self.runs.append(run)
+            self.runs[data_file_stem] = run
+        self.runs = {k: v for k, v in sorted(self.runs.items(), key=lambda item: item[0])}
         self.output_path = pathlib.Path.home() / 'output'
         self.output_path.mkdir(parents=True,exist_ok=True)
 
     def plot(self):
         subplot_index = 1
-        for run in self.runs:
+        for name, run in self.runs.items():
             plt.subplot(4,2,subplot_index)
             subplot_index += 1
             plt.plot(run[:,0],run[:,1])
+            plt.title(name)
         plt.tight_layout()
         plot_path = self.output_path / 'plots.png'
         plt.savefig(plot_path)
